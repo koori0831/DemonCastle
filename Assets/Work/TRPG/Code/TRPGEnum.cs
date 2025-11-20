53294d9ad256bb8646f3e4469af5a3e39f838c64
koori0831 <fuyunokoori0831@gmail.com>
2025-11-20 19:41:24 +0900

    feat: Rendering Setting and Template Scene Setting

---
+++ Assets/Work/TRPG/Code/TRPGEnum.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Work.TRPG.Code
+{
+    public enum CheckResult
+    {
+        CriticalSuccess, // 대성공
+        Success,         // 성공
+        Failure,         // 실패
+        Fumble           // 대실패
+    }
+}
