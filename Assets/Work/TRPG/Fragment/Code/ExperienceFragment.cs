b2d1a8531d0db94f3985b2c2d61c3b2f16f3b3ef
koori0831 <fuyunokoori0831@gmail.com>
2025-12-11 17:22:22 +0900

    fix : Project Convention

---
+++ Assets/Work/TRPG/Fragment/Code/ExperienceFragment.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+namespace Work.TRPG.Fragment
+{
+	[CreateAssetMenu(fileName = "ExperienceFragment", menuName = "SO/TRPG/Fragment/ExperienceFragment", order = 1)]
+	public class ExperienceFragment: Fragment
+	{
+    }
+}
---
+++ Assets/Work/TRPG/Fragment/Code/PersonalValueFragment.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+namespace Work.TRPG.Fragment
+{
+	[CreateAssetMenu(fileName = "PersonalValueFragment", menuName = "SO/TRPG/Fragment/PersonalValueFragment", order = 2)]
+    public class PersonalValueFragment: Fragment
+	{
+	}
+}
