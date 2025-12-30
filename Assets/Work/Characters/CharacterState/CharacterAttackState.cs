6ae8628b4af3dceb50b6c5fd8a6f7c09bf04c25e
gominsu08 <minsu080618@gmail.com>
2025-12-30 15:45:10 +0900

    fix : animation blend tree in player move

--- Assets/Work/Characters/CharacterState/CharacterAttackState.cs
+++ Assets/Work/Characters/CharacterState/CharacterAttackState.cs
@@ -1,4 +1,6 @@
-﻿using Work.Characters.Code;
+﻿using UnityEngine;
+using Work.Characters.Code;
+using Work.Characters.Events;
 using Work.Entities;
 
 namespace Work.Characters.CharacterState
@@ -36,6 +38,19 @@ namespace Work.Characters.CharacterState
             _attackCompo.AddAttackCount();
             _movementCompo.SetCanMove(false);
             _movementCompo.SetMultiplier(1);
+        }
+
+        protected override void MoveHandler(CharacterMoveEvent evt)
+        {
+            if(evt.MoveDirection == Vector3.zero)
+            {
+                _animator.Anim.SetLayerWeight(1, 0f);
+            }
+            else
+            {
+                _animator.Anim.SetLayerWeight(1, 1f);
+            }
+            
         }
     }
 }
--- Assets/Work/Characters/Code/CharacterAnimatorCompo.cs
+++ Assets/Work/Characters/Code/CharacterAnimatorCompo.cs
@@ -35,5 +35,6 @@ namespace Work.Characters.Code
 
 
 
+
     }
 }
