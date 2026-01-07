3294b169de19b0dab016777675a0c04287e991de
koori0831 <fuyunokoori0831@gmail.com>
2026-01-07 16:37:03 +0900

    feat : Character Data Handling

---
+++ Assets/Work/Characters/Stats/Code/StatBlock.cs
@@ -0,0 +1,35 @@
+﻿using Work.Characters.Code;
+
+namespace Work.Characters.Stats.Code
+{
+    public struct StatBlock
+    {
+        int STR, INT, LUK, DEF, SAN, CON, DEX;
+
+        public int GetStatValue(CharacterStatEnum stat)
+        {
+            return stat switch
+            {
+                CharacterStatEnum.STR => STR,
+                CharacterStatEnum.INT => INT,
+                CharacterStatEnum.LUK => LUK,
+                CharacterStatEnum.DEF => DEF,
+                CharacterStatEnum.SAN => SAN,
+                CharacterStatEnum.CON => CON,
+                CharacterStatEnum.DEX => DEX,
+                _ => 0,
+            };
+        }
+
+        public static StatBlock operator +(StatBlock a, StatBlock b) => new()
+        {
+            STR = a.STR + b.STR,
+            INT = a.INT + b.INT,
+            LUK = a.LUK + b.LUK,
+            DEF = a.DEF + b.DEF,
+            SAN = a.SAN + b.SAN,
+            CON = a.CON + b.CON,
+            DEX = a.DEX + b.DEX,
+        };
+    }
+}
--- Assets/Work/TRPG/Fragment/Code/PersonalValueFragment.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-
-namespace Work.TRPG.Fragment
-{
-	[CreateAssetMenu(fileName = "PersonalValueFragment", menuName = "SO/TRPG/Fragment/PersonalValueFragment", order = 2)]
-    public class PersonalValueFragment: Fragment
-	{
-	}
-}
